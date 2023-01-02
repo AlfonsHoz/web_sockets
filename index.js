269a27c24b63669b0985649c5c1e949aec5cf8f1
AlfonsHoz <90301382+AlfonsHoz@users.noreply.github.com>
2023-01-02 10:28:04 -0500

    new socket event
    new socket event

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const express = require("express");
-const path = require("path");
-require("dotenv").config();
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
 
+// App de Express
 const app = express();
-const port = process.env.PORT;
 
-const publicPath = path.resolve(__dirname, "public");
+// Node Server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
 
-app.use(express.static(publicPath));
-app.listen(port, (error) => {
-  if (error) throw new Error(error);
-  else console.log(`Example app listening on port ${port}!!!!!`);
+
+
+
+// Path público
+const publicPath = path.resolve( __dirname, 'public' );
+app.use( express.static( publicPath ) );
+
+
+
+
+
+server.listen( process.env.PORT, ( err ) => {
+
+    if ( err ) throw new Error(err);
+
+    console.log('Servidor corriendo en puerto', process.env.PORT );
+
 });
+
+
